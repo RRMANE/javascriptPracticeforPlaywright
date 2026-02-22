a2c338673e0a50be458566ea422225771fc54a2e
RRMANE <rohitmane53@gmail.com>
2026-02-22 22:45:26 +0530

    dataType3Practice

---
+++ dataTypes3.js
@@ -0,0 +1,17 @@
+// let a = 10;
+// let b = 20;
+// console.log(`Total is ${a + b}`);
+
+let math = `2 + 2= ${2 +2}`;
+console.log(math); 
+
+//I want to store my path of the file
+let path = "D:\\Playwrite Testing\\test.js";
+console.log(path);
+//store urls
+
+let address = "https://app.resure.realestate/";
+console.log(address);
+
+console.log(null === undefined);
+console.log(null == undefined);
