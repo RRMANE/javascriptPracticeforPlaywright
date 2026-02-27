f997de27cfec0d8967bb105477b898f5e8dec7ef
RRMANE <rohitmane53@gmail.com>
2026-02-27 07:44:27 +0530

    if_else Practice4

---
+++ 4if_else.js
@@ -0,0 +1,18 @@
+let marks= 90;
+
+if (marks>= 90) {
+    console.log("Grade: A");
+}
+else if (marks >= 80) {
+    console.log("Grade: B");
+}
+else if (marks >= 65) {
+    console.log("Grade: C");
+}
+else if (marks>= 50) {
+    console.log("Grade: D");
+}
+else if (marks >= 35) {
+    console.log ("You are Pass");
+}
+else {console.log("You are Fail. Try again!");}
