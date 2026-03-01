07d1ced7d0d028f63a977aaf29eec4fbc8b08184
RRMANE <rohitmane53@gmail.com>
2026-03-01 06:46:02 +0530

    Increament and Decriment

---
+++ forLoop_2.js
@@ -0,0 +1,23 @@
+// let a = 10;
+// let b = a++;
+// console.log("Value Of a:", a);
+// console.log("Value Of b:", b);
+
+// let a = 10;
+// let b = ++a;
+// console.log("Value Of a:", a);
+// console.log("Value Of b:", b);
+// console.log("Value Of a:", a);
+
+// let a = 10;
+// let b = a--;
+// console.log("Value Of b:", b);
+// console.log("Value of a:", a);
+// console.log("Value of b:", b);
+
+let a = 10;
+let b = --a;
+console.log("Value of a:", a);
+console.log("Value of b:", b);
+console.log("Value of a:", a);
+console.log("Value of b:", b);
