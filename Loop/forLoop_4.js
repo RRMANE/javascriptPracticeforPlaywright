a2428833ca5ad9439a6420c0293fc919388de3cf
RRMANE <rohitmane53@gmail.com>
2026-03-05 20:54:34 +0530

    For loop 4

---
+++ Loop/forLoop_4.js
@@ -0,0 +1,6 @@
+for(Sima= 0; Sima<=18; Sima++) {
+    if(Sima == 18){
+        console.log("Sima will get a Laptop gift on her 18th birthday")
+    }
+    else {console.log("Sima will get a babt dall as a gift on her birthday")}
+}
