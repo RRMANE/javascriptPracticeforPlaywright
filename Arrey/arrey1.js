16d1178a85f757029b2e8e63b1bbe1b30f9a6f07
RRMANE <rohitmane53@gmail.com>
2026-03-09 07:35:02 +0530

    My 1st Arrey

---
+++ Arrey/arrey1.js
@@ -0,0 +1,8 @@
+let arrey = [1, 2, 3, 4, 5];
+console.log(arrey);
+
+let fruits = ["apple", "banana", "Mango", "Grapes"];
+console.log(fruits);
+
+let mixedArrey = [1, "Hello", true, null, undefined];
+console.log(mixedArrey);
