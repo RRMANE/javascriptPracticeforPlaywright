1669b005f5a049ba692fee6d968532d88e9db9f0
RRMANE <rohitmane53@gmail.com>
2026-03-11 10:18:09 +0530

    Access Modify Update

---
+++ Array/array_Access_Modify.js
@@ -0,0 +1,15 @@
+let fruits = ["apple", "Mango", "Banana", "Pupaya"];
+console.log(fruits)
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[4]);
+
+fruits[1] = "Grapes";
+console.log(fruits);
+console.log(fruits.at(-2));
+
+fruits[2] = "Almond";
+console.log(fruits)
+console.log(fruits.length);
