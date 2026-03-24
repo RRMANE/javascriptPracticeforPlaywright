9fe52ca7610d447c320ebdd939e4fdbc27a57467
RRMANE <rohitmane53@gmail.com>
2026-03-24 15:25:52 +0530

    Add object1.js only

---
+++ Objects/object1.js
@@ -0,0 +1,7 @@
+//Objects>> Key Value pair
+let student = { Name:"John", Age: 45};
+let student2= {Name:"Pramod"};
+let student3= {Name:"Pramod", Age: 40, Phone: 4565986465};
+console.log(student);
+console.log(student2);
+console.log(student3);
